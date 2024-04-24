23eb6d442c1ad5c587486c5fc78dcb18b1fffc47
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2024-04-24 10:56:01 +0100

    Some validations registerDTO

--- src/main/java/com/brlopes/dto/registerDTO.java
+++ src/main/java/com/brlopes/dto/registerDTO.java
@@ -1,9 +1,20 @@
 package com.brlopes.dto;
 
 import com.brlopes.Model.enums.LoginRoles;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record registerDTO (@NotBlank String login,@NotBlank String password,@NotNull LoginRoles role){
-}
+public record registerDTO(
+		@NotBlank
+		@Size(min = 4, message = "Login must be at least 4 characters long")
+		@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Login can only contain letters and numbers")
+		String login,
+
+		@NotBlank
+		String password,
+
+		@NotNull
+		LoginRoles role
+) {}

cae6521b940b271ad0ba7a3766a5ecad10837da5
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2024-04-24 11:30:28 +0100

    Check balance of clientId

--- src/main/java/com/brlopes/Controller/ClientController.java
+++ src/main/java/com/brlopes/Controller/ClientController.java
@@ -1,22 +1,5 @@
 package com.brlopes.Controller;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.brlopes.Model.Client;
 import com.brlopes.Model.Login;
 import com.brlopes.Model.enums.LoginRoles;
@@ -25,26 +8,35 @@ import com.brlopes.Service.ClientService;
 import com.brlopes.Service.exceptions.ErrorResponse;
 import com.brlopes.Service.exceptions.NoClientIdException;
 import com.brlopes.dto.ClientDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController implements Serializable {
-    
+
     @Autowired
     private ClientService clientService;
     @Autowired
     private LoginRepo loginRepo;
-    
+
     @GetMapping("/") // Read - NORMAL CLIENT ROLE
     public ResponseEntity<?> findAll() {
         Iterable<Client> list = clientService.findAll();
-        
+
         List<ClientDTO> listDTO = StreamSupport.stream(list.spliterator(), false)
-        .map(client -> new ClientDTO(client.getName(), client.getAge()))
-        .collect(Collectors.toList());
+                .map(client -> new ClientDTO(client.getName(), client.getAge()))
+                .collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
-    
+
     @PostMapping("/add")  // Create - ADMIN ROLE
     public ResponseEntity<ErrorResponse> addClient(@RequestBody Client client){
         client.setRole(LoginRoles.CLIENT);
@@ -57,7 +49,7 @@ public class ClientController implements Serializable {
         }
         return ResponseEntity.ok().build();
     }
-    
+
     @GetMapping("/{id}") // Read - ADMIN ROLE
     public ResponseEntity<?> findById(@PathVariable Long id) {
         try {
@@ -68,14 +60,14 @@ public class ClientController implements Serializable {
             return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
         }
     }
-    
-    
+
+
     @DeleteMapping("/{id}") // Delete (by ID) - ADMIN ROLE
     public ResponseEntity<ErrorResponse> deletebyId(@PathVariable Long id) {
         clientService.deletebyId(id);
         return ResponseEntity.noContent().build();
     }
-    
+
     @PutMapping("/{id}") // Update (by ID) - ADMIN ROLE
     public ResponseEntity<ErrorResponse> updateById(@PathVariable Long id, @RequestBody Client client){
         try {
@@ -86,4 +78,27 @@ public class ClientController implements Serializable {
             return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
         }
     }
+
+
+    /**
+     * This method is used to check the balance of a client by their ID.
+     * It first tries to find the client in the database using the provided ID.
+     * If the client is found, it returns the client's balance wrapped in a ResponseEntity with an HTTP status of 200 (OK).
+     * If the client is not found, it throws a NoClientIdException, which is caught and handled by returning an ErrorResponse
+     * wrapped in a ResponseEntity with an HTTP status of 400 (Bad Request).
+     *
+     * @param id This is the ID of the client whose balance is to be checked.
+     * @return ResponseEntity<?> This returns the client's balance if the client is found, otherwise an ErrorResponse.
+     * @throws NoClientIdException This exception is thrown when the client with the provided ID is not found in the database.
+     */
+    public ResponseEntity<?> checkBalancebyId(Long id) {
+        try {
+            Client client = clientService.findById(id);
+            return ResponseEntity.ok().body(client.getBalance());
+        } catch (NoClientIdException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, "Client not found in DataBase. Register first.");
+            return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+        }
+    }
+
 }
